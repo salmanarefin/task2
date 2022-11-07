326a52fa004e8c5ce66961f142b36a070890dc71
Salman Arefin <115951122+salmanarefin@users.noreply.github.com>
2022-11-08 01:27:03 +0600

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,18 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ int square (int x)
+
+ {
+     return x*x;
+ }
+
+ int main ()
+
+ {
+     int a= 5;
+
+     cout<<square(a)<<endl;
+     return 0;
+ }
